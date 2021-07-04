bec5cf8691d4dde696174d6e5a4c221363a918b2
Stetson2000 <haali-2000@graduate.utm.my>
2021-07-04 08:33:04 +0200

    commit

--- src/events/events.controller.spec.ts
+++ src/events/events.controller.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { EventsController } from './events.controller';
-import { EventsService } from './events.service';
+import { EventsService } from './Events.service';
 
 describe('EventsController', () => {
   let controller: EventsController;
